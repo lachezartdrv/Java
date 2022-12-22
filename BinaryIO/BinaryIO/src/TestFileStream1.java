522746ae8619ed2e53c6bcf3254622483e8be669
Lachezar Todorov <121232198+lachezartdrv@users.noreply.github.com>
2022-12-22 16:58:26 +0200

    Add files via upload

---
+++ BinaryIO/BinaryIO/src/TestFileStream1.java
@@ -0,0 +1,26 @@
+package BinaryIO.src;
+
+import java.io.*;
+
+public class TestFileStream1 {
+    public static void main(String[] args) throws IOException {
+        try (
+                // Create an output stream to the file
+                FileOutputStream output = new FileOutputStream("temp.dat");
+        ) {
+            // Output values to the file
+            for (int i = 1; i <= 10; i++)
+                output.write(i);
+        }
+
+        try (
+// Create an input stream for the file
+                FileInputStream input = new FileInputStream("temp.dat");
+        ) {
+            // Read values from the file
+            int value;
+            while ((value = input.read()) != -1)
+                System.out.print(value + " ");
+        }
+    }
+}
